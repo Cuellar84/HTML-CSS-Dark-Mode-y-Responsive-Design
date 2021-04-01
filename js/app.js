77c9dc22ba8079bb0c6c98b9a009ce614bff1c1d
Cuellar84 <raymundo.cuellart@gmail.com>
2021-03-31 20:33:55 -0600

    Agregando el proyecto

---
+++ js/app.js
@@ -0,0 +1,10 @@
+console.log()
+const checkbox = document.querySelector('#checkbox')
+if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+    console.log(true)
+    checkbox.setAttribute('checked', true)
+}
+
+checkbox.addEventListener('change', function(event) {  
+        document.body.classList.toggle('is-dark-mode')  
+})
